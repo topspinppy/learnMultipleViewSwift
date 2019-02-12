8c145407b6a00e33cdba51975e5062274ca635de
Admin <admin@MAC4.local>
2019-02-12 10:38:15 +0700

    first commit

---
+++ learnMultipleView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  learnMultipleView
+//
+//  Created by Admin on 12/2/2562 BE.
+//  Copyright © 2562 KMUTNB. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationItem.title = "Menu"
+//        self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.add, target: self, action: Selector("addItem"))
+    }
+}
+
